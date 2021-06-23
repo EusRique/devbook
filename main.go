72e979acbfa2710f4563d33cc879679be2530307
Henrique <hiquesc_@hotmail.com>
2021-06-22 21:54:58 -0300

    feat: create package main and route

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"devbook/src/router"
+	"log"
+	"net/http"
+)
+
 func main() {
 	println("Hello Worlds!!!")
+
+	r := router.Gerar()
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
